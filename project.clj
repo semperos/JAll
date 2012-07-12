016edf96743e47829ad83a4e69da32f4851de9c2
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-07-12 02:06:19 -0400

    Complete package inclusion and change namespaces to be compiled accordingly

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jall "0.1.0-SNAPSHOT"
+(defproject com.semperos/jall "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -9,5 +9,5 @@
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojure/tools.logging "0.2.4"]]
   :dev-dependencies [[expectations "1.4.4"]]
-  :aot [jall.lang.Compiler]
-  :main jall.lang.Compiler)
+  :aot [com.semperos.jall.lang.Compiler]
+  :main com.semperos.jall.lang.Compiler)
